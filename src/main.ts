d29bd963c338a21545239aca6d30e625d55806b8
SolmundSolv <konradqxd@gmail.com>
2023-03-02 13:30:16 +0100

    finaly

--- src/main.ts
+++ src/main.ts
@@ -22,17 +22,5 @@ async function bootstrap() {
   );
   await app.listen(3001);
   console.log(`Application is running on: ${await app.getUrl()}`);
-  const https = await NestFactory.create(AppModule, {
-    httpsOptions,
-    logger: ['error', 'warn', 'debug', 'log', 'verbose'],
-  });
-  https.enableCors();
-  https.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
-  await https.listen(443);
-  console.log(`Application is running on: ${await https.getUrl()}`);
 }
 bootstrap();
